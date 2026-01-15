a51f5b91b3b02572a1fdfcf72f5e223c988b7d16
JuanpyX <78767767+JuanpyX98@users.noreply.github.com>
2026-01-15 18:42:10 -0300

    1.0.3

--- src/app/page.js
+++ src/app/page.js
@@ -73,7 +73,7 @@ export default function Home() {
   ]);
 
   let [mostrarNavegacionResponsive, setMostrarNavegacionResponsive] =
-    useState(true);
+    useState(false);
 
   function abrirNavResp() {
     setMostrarNavegacionResponsive(!mostrarNavegacionResponsive);
